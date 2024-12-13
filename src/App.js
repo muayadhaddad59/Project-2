4ad0aab6f566750b266e8ba087595ded875f3ac4
hussamhijazi <hussamhijazi66@gmail.com>
2024-12-13 01:59:22 -0500

    15th commit - imported search to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import Hero from './components/hero/Hero'
 import Destinations from './components/destinations/Destinations'
+import Search from './components/search/Search';
 
 function App() {
   return (
@@ -9,6 +10,16 @@ function App() {
       <Navbar />
       <Hero />
       <Destinations  />
+      <Search 
+            destinations={[
+                "Banff National Park, Alberta",
+            ]}
+            promoDetails={{
+                discountText: "GET AN ADDITIONAL 25% OFF",
+                timeLeft: "12 HOURS LEFT!",
+                offerText: "VIEW ALL CURRENT OFFERS"
+            }}
+        />;
 
     </div>
   );
