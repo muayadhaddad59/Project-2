61e87be31831640f5bb4ecedae705f2a14d1761c
hussamhijazi <hussamhijazi66@gmail.com>
2024-12-13 01:56:27 -0500

    12th commit - imported destination to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import Hero from './components/hero/Hero'
+import Destinations from './components/destinations/Destinations'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Destinations  />
 
     </div>
   );
